88680672cfca5cef281cda431c2a89d7991ee2bb
CLR60 <79085963+CLR60@users.noreply.github.com>
2021-06-17 14:23:03 +0200

    modif carousel

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
 
 	<section id="templates">
 		<div class="container carouTemplate">
-	  		<h2 class="choix">trouvez un titre  </h2>
+	  		<h2 class="choix">Site vitrine  </h2>
 
 		
 		
@@ -74,7 +74,7 @@
 					    <div class="carousel-item active">
 					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
 					      <div class="carousel-caption d-none d-md-block">
-					        <h5>Site Vitrine</h5>
+					        <h5>Site Vitrine 1</h5>
 					        <p>Template pour votre site vitrine</p>
 					        <a href="">Prévisualiser</a>
 					      </div>
@@ -83,32 +83,32 @@
 					    <div class="carousel-item">
 					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
 					      <div class="carousel-caption d-none d-md-block">
-					        <h5>Site E-commerce</h5>
-					        <p>Template pour votre site de e-commerce</p>
+					         <h5>Site vitrine 2</h5>
+					        <p>Template pour votre site vitrine</p>
 					        <a href="">Prévisualiser</a>
 					      </div>
 					    </div>
 					    <div class="carousel-item">
 					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
 					      <div class="carousel-caption d-none d-md-block">
-					        <h5>Blog</h5>
-					        <p>Template pour votre Blog</p>
+					         <h5>Site Vitrine 3</h5>
+					        <p>Template pour votre site vitrine</p>
 					        <a href="">Prévisualiser</a>
 					      </div>
 					    </div>
 					    <div class="carousel-item">
 					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
 					      <div class="carousel-caption d-none d-md-block">
-					        <h5>Site Wiki</h5>
-					        <p>Template pour votre Wiki.</p>
+					         <h5>Site Vitrine 4</h5>
+					        <p>Template pour votre site vitrine</p>
 					        <a href="">Prévisualiser</a>
 					      </div>
 					    </div>
 					    <div class="carousel-item">
 					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
 					      <div class="carousel-caption d-none d-md-block">
-					        <h5>Third slide label</h5>
-					        <p>Some representative placeholder content for the third slide.</p>
+					         <h5>Site Vitrine 5</h5>
+					        <p>Template pour votre site vitrine</p>
 					        <a href="">Prévisualiser</a>
 					      </div>
 					    </div>
@@ -118,6 +118,219 @@
 				    <span class="visually-hidden">Previous</span>
 				  </button> -->
 		  			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+		    			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    			<span class="visually-hidden">Next</span>
+		  			</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="container carouTemplate">
+	  		<h2 class="choix">Site de E-commerce  </h2>
+
+		
+		
+		
+			<div class="container bg-dark">
+				<div id="carouselExampleCaptions2" class="carousel slide" data-bs-ride="carousel">
+	  				<div class="carousel-indicators">
+					    <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="3" aria-label="Slide 4"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide-to="4" aria-label="Slide 5"></button>
+	  				</div>
+	  <!-- bloc a copier  -->
+					<div class="carousel-inner">
+					    <div class="carousel-item active">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Site E-commerce 1</h5>
+					        <p>Template pour votre site de e-commerce</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					    <!-- fin de bloc a copier -->
+					    <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Site E-commerce 2</h5>
+					        <p>Template pour votre site de e-commerce</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					    <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Site E-commerce 3</h5>
+					        <p>Template pour votre site de e-commerce</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					    <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Site E-commerce 4</h5>
+					        <p>Template pour votre site de e-commerce</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					    <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Site E-commerce 5</h5>
+					        <p>Template pour votre site de e-commerce</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					</div>
+				  <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				    <span class="visually-hidden">Previous</span>
+				  </button> -->
+		  			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions2" data-bs-slide="next">
+		    			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    			<span class="visually-hidden">Next</span>
+		  			</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="container carouTemplate">
+	  		<h2 class="choix">Blog  </h2>
+
+		
+		
+		
+			<div class="container bg-dark">
+				<div id="carouselExampleCaptions3" class="carousel slide" data-bs-ride="carousel">
+	  				<div class="carousel-indicators">
+					    <button type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide-to="1" aria-label="Slide 2"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide-to="2" aria-label="Slide 3"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide-to="3" aria-label="Slide 4"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide-to="4" aria-label="Slide 5"></button>
+	  				</div>
+	  <!-- bloc a copier  -->
+					<div class="carousel-inner">
+					    <div class="carousel-item active">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Blog 1</h5>
+					        <p>Template pour votre Blog</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					    <!-- fin de bloc a copier -->
+					    <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Blog 2</h5>
+					        <p>Template pour votre Blog</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					    <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Blog 3</h5>
+					        <p>Template pour votre Blog</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					    <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Blog 4</h5>
+					        <p>Template pour votre Blog</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					    <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Blog 5</h5>
+					        <p>Template pour votre Blog</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					</div>
+				  <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				    <span class="visually-hidden">Previous</span>
+				  </button> -->
+		  			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions3" data-bs-slide="next">
+		    			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    			<span class="visually-hidden">Next</span>
+		  			</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="container carouTemplate">
+	  		<h2 class="choix">Site Wiki  </h2>
+
+		
+		
+		
+			<div class="container bg-dark">
+				<div id="carouselExampleCaptions4" class="carousel slide" data-bs-ride="carousel">
+	  				<div class="carousel-indicators">
+					    <button type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide-to="1" aria-label="Slide 2"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide-to="2" aria-label="Slide 3"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide-to="3" aria-label="Slide 4"></button>
+					    <button type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide-to="4" aria-label="Slide 5"></button>
+	  				</div>
+	  <!-- bloc a copier  -->
+					<div class="carousel-inner">
+					    <div class="carousel-item active">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					        <h5>Site Wiki 1</h5>
+					        <p>Template pour votre Wiki.</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					    <!-- fin de bloc a copier -->
+					    <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					       <h5>Site Wiki 2</h5>
+					        <p>Template pour votre Wiki.</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					     <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					       <h5>Site Wiki 3</h5>
+					        <p>Template pour votre Wiki.</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					     <div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					       <h5>Site Wiki 4</h5>
+					        <p>Template pour votre Wiki.</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+ 						<div class="carousel-item">
+					      <img src="images/img1.jpg" class="d-block w-90" alt="...">
+					      <div class="carousel-caption d-none d-md-block">
+					       <h5>Site Wiki 5</h5>
+					        <p>Template pour votre Wiki.</p>
+					        <a href="">Prévisualiser</a>
+					      </div>
+					    </div>
+					</div>
+				  <!-- <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				    <span class="visually-hidden">Previous</span>
+				  </button> -->
+		  			<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions4" data-bs-slide="next">
 		    			<span class="carousel-control-next-icon" aria-hidden="true"></span>
 		    			<span class="visually-hidden">Next</span>
 		  			</button>
